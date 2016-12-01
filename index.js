c338e38b225b25a64ac9198eecae37943bb0bc80
Paul Williams <paul@pdwilliams.net>
2016-12-01 16:40:10 +1100

    more boilerplate

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 
+// Settings
 app.set('view engine', 'ejs')
 
+// Middleware
+app.use(express.static('public'))
+
+// Routes
 app.get('/', function(req, res) {
   res.render('index')
 })
 
+// Start server
 app.listen(3030)
