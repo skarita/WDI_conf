f3ad8c10377593e463a4ee89afcdcc7a9423d142
Paul Williams <paul@pdwilliams.net>
2016-12-02 12:02:18 +1100

    jquery module

--- client/index.js
+++ client/index.js
@@ -1,7 +1,10 @@
 require('./styles/main.scss')
+const $ = require('jQuery')
 
-console.log('testing');
+console.log('testing')
 
 var jason3 = () => {
   return true
 }
+
+$('#root').text('jquery test?')
