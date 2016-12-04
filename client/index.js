eec4ff1375c9fc6a56c98d0416d1fa510c0e7bfd
paulmorello <paulmorello1@gmail.com>
2016-12-04 15:43:07 +1100

    Added reset css file

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 require('./styles/main.scss')
+require('./styles/reset.scss')
 const renderLandingPage = require('./views/landing-page.js')
 const $ = require('jquery')
 const stripeResponseHandler = require('./scripts/stripe-payment.js')
