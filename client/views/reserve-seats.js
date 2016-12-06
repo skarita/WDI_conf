3245381309589df15101b2f7fad27a6f4c5871b3
Paul Williams <paul@pdwilliams.net>
2016-12-06 14:22:18 +1100

    loop through row/seat data

--- client/views/reserve-seats.js
+++ client/views/reserve-seats.js
@@ -1,6 +1,5 @@
 const $ = require('jquery')
 
-
 var renderSeats = function() {
 
   var talk = {
@@ -16,17 +15,17 @@ var renderSeats = function() {
     ]
   }
 
-
   $('<div id="wrapper">').appendTo('#root');
   $('<h5>').text('Payment Sucessful').appendTo('#wrapper');
   $('<p>').text('Would you like to reserve seating?').appendTo('#wrapper');
   $('<button>').text('Skip').appendTo('#wrapper');
   $('<div>').text('stage').appendTo('#wrapper');
-  $.each(talk.seats, function(v) {
-    var $row = $('<div>');
-    $.each(v, function(seat, status) {
-      // $('<span>').text(seat + " " + status).appendTo($row);
-    });
+
+  $.each(talk.seats, function(i, obj) {
+    var $row = $('<div id="test">')
+    Object.keys(obj).forEach(function(key) {
+      $('<span>').text(key + " " + obj[key]).appendTo($row);
+    })
     $row.appendTo('#wrapper');
   });
 
