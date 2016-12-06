287a232d0dd0fffc6dec0ca08efc64624fcd57ed
paulmorello <paulmorello1@gmail.com>
2016-12-06 16:47:59 +1100

    speaker section added to home

--- client/views/landing-page.js
+++ client/views/landing-page.js
@@ -1,37 +1,19 @@
 const $ = require('jquery')
 const renderPaymentForm = require('./payment-form.js')
-
+const renderHomeSection = require('./home-section.js')
+const renderSpeakerSection = require('./speaker-section.js')
 
 var renderLandingPage = function() {
 
-  $('<div class="home-wrapper">').appendTo('#root')
-  $('<section class="home-top-wrapper">').appendTo('.home-wrapper')
-  $('<nav class="home-nav">').appendTo('.home-top-wrapper')
-  $('<ul>').appendTo('.home-nav')
-  $('<li> <a id="first">').appendTo('ul')
-  $('<li> <a id="second">').appendTo('ul')
-  $('<li> <a id="third">').appendTo('ul')
-  $('<li class="nav-ticket-btn"> <a id="fourth">').appendTo('ul')
-  $('#first').text('Speakers')
-  $('#second').text('Sponsors')
-  $('#third').text('Location')
-  $('#fourth').text('Buy Tickets')
+  renderHomeSection();
 
-  $('<h1>').text('WDI CONF 2017').appendTo('.home-top-wrapper')
-
-  $('<section class="home-middle-wrapper">').appendTo('.home-wrapper')
-  $('<div class="home-middle-location">').appendTo('.home-middle-wrapper')
-  $('<h2>').text('Melbourne').appendTo('.home-middle-location')
-  $('<h3>').text('General Assembly').appendTo('.home-middle-location')
-
-  $('<a class="buy-ticket-btn">').text('Buy Tickets').appendTo('.home-wrapper')
+  renderSpeakerSection();
 
   $('.buy-ticket-btn, #fourth').click(function() {
 
     $('<div id="modal-wrapper" class="modal">').appendTo('#root');
     $('<div id="payment-form-modal" class="modal">').appendTo('#modal-wrapper');
       renderPaymentForm()
-
   })
 
 }
---
+++ client/views/speaker-section.js
@@ -0,0 +1,15 @@
+const $ = require('jquery')
+
+var renderSpeakerSection = function() {
+
+  $('<div class="speaker-wrapper">').appendTo('.home-wrapper');
+
+  $('<section class="speaker-main">').appendTo('.speaker-wrapper');
+
+
+
+
+
+}
+
+module.exports = renderSpeakerSection;
